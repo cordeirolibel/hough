ef62aa587eb5442f00ad8dc392b5c5d4e3276e28
cordeirolibel <gustavolibel@gmail.com>
2018-05-09 10:48:35 -0300

    add circles

--- lines_generator.py
+++ lines_generator.py
@@ -7,9 +7,40 @@
 
 from matplotlib import pyplot as plt 
 from skimage.exposure import equalize_hist
-from skimage.draw import line_aa
+from skimage.draw import line_aa,ellipse_perimeter
 import numpy as np
 import cv2
+
+
+## Parameters
+
+
+size = (1280,720)
+
+#lines
+delta_lines = [3,30]
+aplha_lines = [0.7,1]
+
+#ellipses
+delta_ellipses = [3,10]
+aplha_ellipses = [0.7,1]
+radius_ellipses = [0.1,0.7] #1 = size, ex:1280,720
+
+#texture
+n_textures = 2
+alpha_textures = [0.05, 0.2] #max=1
+
+#binary noise
+binary_noise_probability = 0.003 #max=1
+
+#smoothing
+smooth_delta_kernel = [3,11] #[3,7] => (3,3)or(5,5)or(7,7)
+
+#simple noise
+noise_delta_sigma = [-10,10] #max=255
+
+
+
 
 # ## Aux function
 
@@ -60,6 +91,42 @@ def add_lines(im,delta_lines,aplha_lines):
     
     return np.array(lines), norm(im)
 
+
+# ## Add Ellipses
+
+def add_ellipses(im,delta_ellipses,radius_ellipses,aplha_ellipses):
+    im = im.copy()
+
+    #Parameters
+    size = im.shape
+    #d_max = np.floor(np.sqrt(size[0]**2+size[1]**2))-10
+    n_ellipses = np.random.randint(delta_ellipses[0],delta_ellipses[1])
+    
+    #add ellipses
+    for e in range(n_ellipses):
+
+        #random center
+        center_x = np.random.randint(0,size[1])
+        center_y = np.random.randint(0,size[0])
+        
+        #random radius
+        radius_x = np.random.random()*(size[1]*radius_ellipses[1]-size[1]*radius_ellipses[0])\
+                   +size[1]*radius_ellipses[0]
+        radius_y = np.random.random()*(size[0]*radius_ellipses[1]-size[0]*radius_ellipses[0])\
+                   +size[0]*radius_ellipses[0]
+        radius_x = int(radius_x)
+        radius_y = int(radius_y)
+        
+        #random alpha
+        alpha = np.random.random()*(aplha_ellipses[1]-aplha_ellipses[0])+aplha_ellipses[0]
+        
+        #create ellipse
+        pixels_y, pixels_x = ellipse_perimeter(center_y, center_x, radius_y, radius_x, shape = size)
+        vals = im[pixels_y,pixels_x]+255*alpha
+        vals[vals>255] = 255
+        im[pixels_y,pixels_x] = vals
+
+    return norm(im)
 
 # ## Texture
 # Load all textures:
@@ -173,14 +240,17 @@ def equalize(im):
 # In[17]:
 
 def im_generator(n_images=1,
-                 size = (1280,720),
-                 delta_lines = [3,30],
-                 aplha_lines = [0.5,1],
-                 n_textures = 3,
-                 alpha_textures = [0.05, 0.2],
-                 binary_noise_probability = 0.005,
-                 smooth_delta_kernel = [3,11],
-                 noise_delta_sigma = [-30,30]):
+                 size = size,
+                 delta_lines = delta_lines,
+                 aplha_lines = aplha_lines,
+                 delta_ellipses = delta_ellipses,
+                 radius_ellipses = radius_ellipses,
+                 aplha_ellipses = aplha_ellipses,                 
+                 n_textures = n_textures,
+                 alpha_textures = alpha_textures,
+                 binary_noise_probability = binary_noise_probability,
+                 smooth_delta_kernel = smooth_delta_kernel,
+                 noise_delta_sigma = noise_delta_sigma):
     
     #load textures
     ims_texture = load_textures(n_textures,size)
@@ -193,7 +263,10 @@ def im_generator(n_images=1,
 
         #Add Lines
         lines,im = add_lines(im,delta_lines,aplha_lines)
-
+        
+        #Add Ellipses
+        im = add_ellipses(im,delta_ellipses,radius_ellipses,aplha_ellipses)
+        
         #Add Texture
         im = add_texture(im, ims_texture,alpha_textures)
 
